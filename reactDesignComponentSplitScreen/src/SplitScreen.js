71e15183317286cb53620c7d5dc2e4979b42277f
Kyle Mitchell <kylethacoder@gmail.com>
2023-11-17 10:29:28 -0800

    updated

---
+++ reactDesignComponentSplitScreen/src/SplitScreen.js
@@ -0,0 +1,12 @@
+export const SplitScreen = ({ left: Left, right: Right }) => {
+  return (
+    <div>
+      <div>
+        <Left></Left>
+      </div>
+      <div>
+        <Right></Right>
+      </div>
+    </div>
+  );
+};
