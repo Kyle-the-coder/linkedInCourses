b6e2468f2ae4ee33ddb1d9c96d06d8919d3320d0
Kyle Mitchell <kylethacoder@gmail.com>
2023-11-17 10:40:09 -0800

    updated

--- reactDesignComponentSplitScreen/src/App.jsx
+++ reactDesignComponentSplitScreen/src/App.jsx
@@ -1,9 +1,22 @@
 import { useState } from "react";
+import { SplitScreen } from "./SplitScreen";
+
+const LeftHandComponent = () => {
+  return <h1 style={{ backgroundColor: "green" }}>Left!</h1>;
+};
+const RightHandComponent = () => {
+  return <p style={{ backgroundColor: "purple" }}>right!</p>;
+};
 
 function App() {
   return (
     <>
-      <h1>hello</h1>
+      <SplitScreen
+        left={LeftHandComponent}
+        right={RightHandComponent}
+        leftWeight={1}
+        rightWeight={3}
+      />
     </>
   );
 }
