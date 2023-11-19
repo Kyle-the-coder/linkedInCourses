a890c8933c250c410a8bb962d2451805385cb81b
Kyle Mitchell <kylethacoder@gmail.com>
2023-11-19 12:47:04 -0800

    updated

--- reactModal/src/Modal.jsx
+++ reactModal/src/Modal.jsx
@@ -1,9 +1,23 @@
 import { useState } from "react";
 import styled from "styled-components";
 
-const ModalBG = styled.div``;
+const ModalBG = styled.div`
+  position: fixed;
+  z-index: 1;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  overflow: auto;
+  background-color: rgba(0, 0, 0, 0.5);
+`;
 
-const ModalBody = styled.div``;
+const ModalBody = styled.div`
+  background-color: white;
+  margin: 10% auto;
+  padding: 20px;
+  width: 50%;
+`;
 
 export const Modal = ({ children }) => {
   const [shouldShow, setShouldShow] = useState(false);
@@ -12,8 +26,11 @@ export const Modal = ({ children }) => {
     <>
       <button onClick={() => setShouldShow(true)}>Open</button>
       {shouldShow && (
-        <ModalBG>
-          <ModalBody>{children}</ModalBody>
+        <ModalBG onClick={() => setShouldShow(false)}>
+          <ModalBody onClick={() => e.stopPropagation()}>
+            <button onClick={() => setShouldShow(false)}>hide</button>
+            {children}
+          </ModalBody>
         </ModalBG>
       )}
     </>
