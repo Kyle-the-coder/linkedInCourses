a38696f69d618b57fe552b5358acf8cbf009d5bf
Kyle Mitchell <kylethacoder@gmail.com>
2023-11-21 10:59:48 -0800

    updated

--- uncontrolledAndControlledForms/src/UncontrolledForm.jsx
+++ uncontrolledAndControlledForms/src/UncontrolledForm.jsx
@@ -3,16 +3,21 @@ import { useRef } from "react";
 export const UncontrolledForm = () => {
   const nameRef = useRef();
   const ageRef = useRef();
-  const hairRef = useRef();
+  const hairColorRef = useRef();
+
   const handleSubmit = (e) => {
     e.preventDefault();
+    console.log(nameRef.current.value);
+    console.log(ageRef.current.value);
+    console.log(hairColorRef.current.value);
   };
+
   return (
     <form onSubmit={handleSubmit}>
       <input ref={nameRef} name="name" type="text" placeholder="name" />
       <input ref={ageRef} name="age" type="text" placeholder="age" />
       <input
-        ref={hairRef}
+        ref={hairColorRef}
         name="hairColor"
         type="text"
         placeholder="hair color"
