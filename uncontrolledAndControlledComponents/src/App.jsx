25b783984b1bcd6c1767e95cc6641516f7ab6a66
Kyle Mitchell <kylethacoder@gmail.com>
2023-11-22 12:04:26 -0800

    updated

--- uncontrolledAndControlledComponents/src/App.jsx
+++ uncontrolledAndControlledComponents/src/App.jsx
@@ -1,11 +1,25 @@
+import { useState } from "react";
 import { ControlledForm } from "./ControlledForm";
+import { ControlledModal } from "./ControlledModal";
 import { UncontrolledForm } from "./UncontrolledForm";
 import { UncontrolledModal } from "./UncontrolledModal";
 
 function App() {
+  const [shouldShow, setShouldShow] = useState(false);
   return (
     <>
-      <UncontrolledModal />
+      <ControlledModal
+        shouldShow={shouldShow}
+        onRequestClose={() => {
+          alert("Are you sure?");
+          setShouldShow(false);
+        }}
+      >
+        <h1>Hello from Modal!</h1>
+      </ControlledModal>
+      <button onClick={() => setShouldShow(!shouldShow)}>
+        {shouldShow ? "Hide Modal" : "Show Modal"}
+      </button>
     </>
   );
 }
