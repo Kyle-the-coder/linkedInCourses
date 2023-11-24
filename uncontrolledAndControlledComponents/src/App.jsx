a70f63dbffc7236e72178c0592e7af9e10eae895
Kyle Mitchell <kylethacoder@gmail.com>
2023-11-24 11:04:02 -0800

    updated

--- uncontrolledAndControlledComponents/src/App.jsx
+++ uncontrolledAndControlledComponents/src/App.jsx
@@ -4,27 +4,37 @@ import { UncontrolledOnboardingFlow } from "./UncontrolledOnboardingFlow";
 const StepOne = ({ goToNext }) => (
   <>
     <h1>Step 1</h1>
-    <button onClick={goToNext}>Next</button>
+
+    <button onClick={() => goToNext({ name: "Kyle Yurok" })}>Next</button>
   </>
 );
 const StepTwo = ({ goToNext }) => (
   <>
     <h1>Step 2</h1>
-    <button onClick={goToNext}>Next</button>
+    <button onClick={() => goToNext({ age: "32" })}>Next</button>
   </>
 );
-const StepThree = ({ goToNext }) => (
-  <>
-    <h1>Step 3</h1>
-    <button onClick={goToNext}>Next</button>
-  </>
-);
+const StepThree = ({ goToNext }) => {
+  const [value, setValue] = useState("");
+  return (
+    <>
+      <h1>Step 3</h1>
+      <input type="text" onChange={(e) => setValue(e.target.value)} />
+      <button onClick={() => goToNext({ hairColor: value })}>Next</button>
+    </>
+  );
+};
 
 function App() {
   const [shouldShow, setShouldShow] = useState(false);
   return (
     <>
-      <UncontrolledOnboardingFlow>
+      <UncontrolledOnboardingFlow
+        onFinish={(data) => {
+          alert("complete!");
+          console.log(data);
+        }}
+      >
         <StepOne />
         <StepTwo />
         <StepThree />
