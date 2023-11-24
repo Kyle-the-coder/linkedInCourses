a10d8d6c70ec20ba555afd4025697d9ecf74f9d3
Kyle Mitchell <kylethacoder@gmail.com>
2023-11-24 11:04:07 -0800

    updated

--- uncontrolledAndControlledComponents/src/UncontrolledOnboardingFlow.jsx
+++ uncontrolledAndControlledComponents/src/UncontrolledOnboardingFlow.jsx
@@ -3,6 +3,27 @@ import React, { useState } from "react";
 export const UncontrolledOnboardingFlow = ({ children, onFinish }) => {
   const [onboardingData, setOnboardingData] = useState({});
   const [currentIndex, setCurrentIndex] = useState(0);
+
+  const goToNext = (stepData) => {
+    const nextIndex = currentIndex + 1;
+
+    const updatedData = {
+      ...onboardingData,
+      ...stepData,
+    };
+
+    console.log(updatedData);
+    if (nextIndex < children.length) {
+      setCurrentIndex(nextIndex);
+    } else {
+      onFinish(updatedData);
+    }
+    setOnboardingData(updatedData);
+  };
+
   const currentChild = React.Children.toArray(children)[currentIndex];
-  return <>{currentChild}</>;
+  if (React.isValidElement(currentChild)) {
+    return React.cloneElement(currentChild, { goToNext });
+  }
+  return currentChild;
 };
