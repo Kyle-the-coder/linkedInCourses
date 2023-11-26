7236b52cc60d912fcc87494ffea83cd5a80d15c0
Kyle Mitchell <kylethacoder@gmail.com>
2023-11-26 11:57:26 -0800

    updated

--- higherOrderComponents/src/App.jsx
+++ higherOrderComponents/src/App.jsx
@@ -1,5 +1,10 @@
+import { printProps } from "./H.O.C./printProps";
+import { UserInfo } from "./UserInfo";
+
+const UserInfoWrapped = printProps(UserInfo);
+
 function App() {
-  return <></>;
+  return <UserInfoWrapped a={1} b="hello" c={{ name: "Kyle" }} />;
 }
 
 export default App;
