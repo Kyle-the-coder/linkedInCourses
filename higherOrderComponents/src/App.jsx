c52709cf66465e88326419b31b3bb70ff5d4641d
Kyle Mitchell <kylethacoder@gmail.com>
2023-11-26 12:10:53 -0800

    updated

--- higherOrderComponents/src/App.jsx
+++ higherOrderComponents/src/App.jsx
@@ -1,10 +1,12 @@
 import { printProps } from "./H.O.C./printProps";
+import { withUser } from "./H.O.C./withUser";
 import { UserInfo } from "./UserInfo";
 
-const UserInfoWrapped = printProps(UserInfo);
+export const addy = "http://localhost:8080";
+const UserInfoWithLoader = withUser(UserInfo, "234");
 
 function App() {
-  return <UserInfoWrapped a={1} b="hello" c={{ name: "Kyle" }} />;
+  return <UserInfoWithLoader />;
 }
 
 export default App;
--- higherOrderComponents/src/H.O.C./withUser.jsx
+++ higherOrderComponents/src/H.O.C./withUser.jsx
@@ -1,1 +1,18 @@
 import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { addy } from "../App";
+
+export const withUser = (Component, userId) => {
+  return (props) => {
+    const [user, setUser] = useState(null);
+
+    useEffect(() => {
+      (async () => {
+        const res = await axios.get(`${addy}/users/${userId}`);
+        setUser(res.data);
+      })();
+    });
+
+    return <Component {...props} user={user} />;
+  };
+};
