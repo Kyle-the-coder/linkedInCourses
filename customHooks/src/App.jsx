08d2b5d19a8fe87563cad9650e108aca3a8a9f64
Kyle Mitchell <kylethacoder@gmail.com>
2023-11-27 12:09:12 -0800

    updated

--- customHooks/src/App.jsx
+++ customHooks/src/App.jsx
@@ -1,7 +1,12 @@
 import { useState } from "react";
-
+import { UserInfo } from "./components/UserInfo";
+export const addy = "http://localhost:8080";
 function App() {
-  return <></>;
+  return (
+    <>
+      <UserInfo />
+    </>
+  );
 }
 
 export default App;
