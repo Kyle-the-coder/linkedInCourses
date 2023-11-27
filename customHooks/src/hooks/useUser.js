6d4b32c97d98077216f59398c4fe41221d7cfe02
Kyle Mitchell <kylethacoder@gmail.com>
2023-11-27 12:13:19 -0800

    updated

---
+++ customHooks/src/hooks/useUser.js
@@ -0,0 +1,16 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { addy } from "../App";
+
+export const useCurrentUser = () => {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      const res = await axios.get(`${addy}/current-user`);
+      setUser(res.data);
+    })();
+  }, []);
+
+  return user;
+};
